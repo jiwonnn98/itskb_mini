2fd81f139e9cca8a2477f5c40e0a869c742f219e
yebin <dev.devin.kr@gmail.com>
2023-08-03 17:09:11 +0900

    feat: 예외 메시지 출력 화면

---
+++ src/view/FailView.java
@@ -0,0 +1,13 @@
+package view;
+
+public class FailView {
+
+    /**
+     * 예외가 발생 시 예외 메세지 출력
+     *
+     * @param message 예외 메시지
+     */
+    public static void errorMessage(String message) {
+        System.out.println(message);
+    }
+}
