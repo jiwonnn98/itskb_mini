4ff2142368edb1daa347131a491cdd625fba044b
jiwon <jiwonno12345@gmail.com>
2023-08-03 11:21:35 +0900

    interface DAO

---
+++ src/common/DBProperties.java
@@ -0,0 +1,10 @@
+package common;
+/**
+ * DB의 설정 정보들 상수로 관리
+ * */
+public interface DBProperties {
+	public static final String DRIVER_NAME ="oracle.jdbc.driver.OracleDriver";
+	String URL="jdbc:oracle:thin:@10.3.4.119:1521:XE";
+	String USER_ID="team5";
+	String USER_PASS="pass";
+}
